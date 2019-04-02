5dd03463291b3da84f999456c6631312fae47904
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-02 02:02:38 +0000

    modify prototype of all method to include cls=None

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
