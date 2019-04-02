ce1723be26516dc97b468b3b66230377279fbfd3
christopherchoe <chchchoe@gmail.com>
2019-04-02 03:17:51 +0000

    initial commit of db storage modifications

--- models/city.py
+++ models/city.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id')
+    name = Column(String(128), nullable=False)
