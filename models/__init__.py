e63f5ba53580d591ea75bea127499f241064d92d
christopherchoe <chchchoe@gmail.com>
2019-04-03 01:54:05 +0000

    fixed db storage env variable check

--- models/__init__.py
+++ models/__init__.py
@@ -10,7 +10,7 @@ from models.review import Review
 import os
 
 
-if 'HBNB_TYPE_STORAGE' in os.environ:
+if os.getenv('HBNB_TYPE_STORAGE') == db:
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
