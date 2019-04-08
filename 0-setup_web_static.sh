ef4045952ab5f7da5b5d4a55c88615fb23edd090
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-08 23:29:22 +0000

    add script that sets up web servers for deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# set up web servers for deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+echo "HTML file to test Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+new_string="location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}"
+file=$"/etc/nginx/sites-available/default"
+sudo sed -i "37i $new_string" $file
+sudo service nginx restart
