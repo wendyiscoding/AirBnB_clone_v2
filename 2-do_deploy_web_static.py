0a329c88f6612a3ba5a1a4d628431e2ce9fc589b
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-09 06:32:30 +0000

    add do_deploy function to distribute archive to web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+distributes an archive to your web servers, using do_deploy
+"""
+from datetime import datetime
+from fabric.api import *
+import os
+
+env.hosts = ['104.196.153.49', '35.231.85.9']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """
+    compresses files, generating a .tgz archive from contents of
+        web_static folder of AirBnB Clone repo
+    Return:
+        name of archive created, or None if unsuccessful
+    """
+    local('mkdir -p versions')
+    zeit = datetime.now().strftime('%Y%m%d%H%M%S')
+    try:
+        local('tar -cvzf versions/web_static_{}.tgz web_static'.format(zeit))
+        return '/versions/web_static_{}.tgz'.format(zeit)
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to your web servers
+    """
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, "/tmp")
+        """modify path as needed"""
+        no_v_or_ext = archive_path[9:-5]
+        no_v = archive_path[9:]
+        pathname = "/data/web_static/releases/{}/".format(no_v_or_ext)
+        run('sudo mkdir -p {}'.format(pathname))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(no_v, pathname))
+        run('sudo rm /tmp/{}'.format(no_v))
+        run('sudo mv {}web_static/* {}'.format(pathname, pathname))
+        run('sudo rm -rf {}web_static'.format(pathname))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -sf {} /data/web_static/current'.format(pathname))
+        return True
+    except:
+        return False
