c202b5b13db2b3ca13cb593011b7915aed40f00d
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-09 00:01:41 +0000

    fix linking in script for setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,10 @@
 # set up web servers for deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo "HTML file to test Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 new_string="location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}"
 file=$"/etc/nginx/sites-available/default"
