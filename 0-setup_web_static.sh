9d02f4cb6f8b9b15ea9a597bdb8472aa16f3e92d
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-09 00:38:05 +0000

    add index in location block for hbnb_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /data/web_static/releases/test/
 echo "HTML file to test Nginx configuration" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-new_string="location /hbnb_static {\n\t\talias /data/web_static/current/\n\t}"
+new_string="location /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm;\n\t}"
 file=$"/etc/nginx/sites-available/default"
 sudo sed -i "29i $new_string" $file
 sudo service nginx restart
