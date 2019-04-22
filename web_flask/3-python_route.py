a1cc99399f3a1fa6a8db234bbfcd9966c9fd9398
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-22 19:30:30 +0000

    initial commit of web_flask files

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+start a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def display_hello():
+    """
+    display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def display_hbnb():
+    """
+    display HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def display_c(text):
+    """
+    display C followed by the value of text variable
+    """
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def display_python(text="is cool"):
+    """
+    display Python followed by the value of text variable with default is cool
+    """
+    return 'Python {}'.format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+start a Flask web application
+"""
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def display_hello():
+    """
+    display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def display_hbnb():
+    """
+    display HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def display_c(text):
+    """
+    display C followed by the value of text variable
+    """
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def display_python(text="is cool"):
+    """
+    display Python followed by the value of text variable with default is cool
+    """
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>")
+def display_number(n):
+    """
+    display n is a number only if n is an integer
+    """
+    return '{} is a number'.format(n)
+
+
+@app.route("/number_template/<int:n>")
+def display_number_template(n):
+    """
+    display HTML page only if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
